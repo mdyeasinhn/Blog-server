fe20a1f26964eb7b5b6a635cfb5e7e8a139c8064
mdyeasinhn <mdyeasinhn@gmail.com>
2025-02-13 10:24:13 +0600

    initial commit and server is runnning on port

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+
+const app: Application = express();
+
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    status: true,
+    message: "server Live",
+  });
+});
+
+console.log(process.cwd());
+
+export default app;
